1a6d2fbfbeb5a6994527cda4e500b95b17a3805f
Ajay <frozenorb3@gmail.com>
2015-09-08 11:30:54 +0000

    Clean up html with partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  
   get 'home', to: 'pages#home'
   get 'some/other', to: 'pages#other'
+  
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
